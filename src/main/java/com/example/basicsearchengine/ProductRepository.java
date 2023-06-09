797a34c7b2d951eb2b681a661e65756c8beeb568
Ashish Sharda <ashishjsharda@gmail.com>
2023-06-09 05:45:14 -0500

    Add ProductRepository interface

---
+++ src/main/java/com/example/basicsearchengine/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.example.basicsearchengine;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ProductRepository extends ElasticsearchRepository<Product, String> {
+
+}
