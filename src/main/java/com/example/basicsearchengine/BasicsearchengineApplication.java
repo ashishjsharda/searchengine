e0d61009b6d5e32e85cdcaad84a08876eaa313cd
Ashish Sharda <ashishjsharda@gmail.com>
2023-06-09 05:37:54 -0500

    first commit

---
+++ src/main/java/com/example/basicsearchengine/BasicsearchengineApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsearchengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsearchengineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicsearchengine/BasicsearchengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsearchengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
