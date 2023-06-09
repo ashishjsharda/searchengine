f42acd69ff861b7738d0e1d4519207a4c4711539
Ashish Sharda <ashishjsharda@gmail.com>
2023-06-09 05:43:34 -0500

    add product podjo

---
+++ src/main/java/com/example/basicsearchengine/Product.java
@@ -0,0 +1,15 @@
+package com.example.basicsearchengine;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "products")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    // Constructor, getters, and setters
+}
+
+
